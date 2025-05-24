b5cf5771651742d03413b201c1ce2a32bd8be121
Vedrion <159234610+vedrion@users.noreply.github.com>
2025-05-25 00:29:26 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,15 +2,24 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Ved'
-description 'Customizable End-of-Life Script for QBCore'
-version '1.0.0'
+description 'Customizable End-of-Life Script for QBCore/ESX/QBox'
+version '1.1.0'
 
--- GitHub: https://github.com/vedrion
 -- Website: https://ved.tebex.io/
+-- GitHub: https://github.com/vedrion
+-- Discord: https://discord.gg/DscAtV7r6J
 
 client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+shared_scripts {
     'config.lua',
-    'client/main.lua'
+    '@ox_lib/init.lua' -- Only required if Config.UseOxLib is set to true
 }
 
 lua54 'yes'
